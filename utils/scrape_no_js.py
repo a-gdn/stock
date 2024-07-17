e45ca4767b7865d3c23df00b510607c92b9eebef
Alexis Gaudin <alexis.gaudin@gadz.org>
2024-07-17 08:58:05 +0200

    handle multiple idnentical class name in bourso fetching

--- utils/scrape_no_js.py
+++ utils/scrape_no_js.py
@@ -16,10 +16,17 @@ async def fetch_theoretical_opening_price(session, ticker):
             if response.status == 200:
                 html = await response.text()
                 soup = BeautifulSoup(html, 'html.parser')
-                span = soup.find('span', class_='c-faceplate__indicative-value')
-                if span:
-                    price_str = span.text.strip().replace(',', '.').replace(' ', '')
-                    theoretical_opening_price = float(price_str)
+                
+                opening_label = soup.find('span', string='Ouverture théorique ')
+
+                if opening_label:
+                    span = opening_label.find_next_sibling('span', class_='c-faceplate__indicative-value')
+                    
+                    if span:
+                        price_str = span.text.strip().replace(',', '.').replace(' ', '')
+                        theoretical_opening_price = float(price_str)
+                    else:
+                        theoretical_opening_price = None
                 else:
                     theoretical_opening_price = None
                 return ticker, theoretical_opening_price
@@ -36,23 +43,29 @@ async def fetch_theoretical_opening_prices(tickers):
         results = await asyncio.gather(*tasks)
         return results
 
+def clean_tickers(tickers):
+    # Remove empty or NaN values from the tickers list
+    return [ticker for ticker in tickers if ticker]
+
 # Function to be called from another file
 def get_theoretical_opening_prices(tickers):
+    tickers = clean_tickers(tickers)
+
     ticker_data = asyncio.run(fetch_theoretical_opening_prices(tickers))
 
     df = pd.DataFrame(ticker_data, columns=['ticker', 'theoretical_opening_price'])
 
     return df
 
+if __name__ == "__main__":
+    file_path = './db/tickers_euronext_regulated_euro_500k€.xlsx'
 
-# file_path = './db/tickers_euronext_regulated_euro_500k€.xlsx'
+    df = pd.read_excel(file_path)
+    tickers = df['bourso'].iloc[1:len(df)+1].values.tolist()
 
-# df = pd.read_excel(file_path)
-# tickers = df['bourso'].iloc[1:872].values.tolist()
+    print(tickers)
 
-# print(tickers)
+    df_prices = get_theoretical_opening_prices(tickers)
 
-# df_prices = get_theoretical_opening_prices(tickers)
-
-# pd.set_option('display.max_rows', None)
-# print(df_prices)
+    pd.set_option('display.max_rows', None)
+    print(df_prices)
